1ec5da42673b5b459a2c4ce0db669f0eab047fd5
EdwinHernandez21 <eherna62@msudenver.edu>
2019-10-07 00:04:02 -0600

    starting project2 again, had to restart all over due to some technical dificulties that I could not solve

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_07_041722) do
+ActiveRecord::Schema.define(version: 2019_10_07_041039) do
 
   create_table "cars", force: :cascade do |t|
     t.integer "make_name_id"
@@ -34,12 +34,6 @@ ActiveRecord::Schema.define(version: 2019_10_07_041722) do
   create_table "makes", force: :cascade do |t|
     t.string "name"
     t.string "country"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "parts", force: :cascade do |t|
-    t.string "part_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
