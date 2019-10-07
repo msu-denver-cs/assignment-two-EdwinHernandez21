5772649a3e3cc9aa77c579c449aedce5d7f6313b
EdwinHernandez21 <eherna62@msudenver.edu>
2019-10-06 22:18:09 -0600

    made parts scaffold

--- db/migrate/20191007041039_create_cars_parts.rb
+++ db/migrate/20191007041039_create_cars_parts.rb
@@ -1,8 +1,8 @@
 class CreateCarsParts < ActiveRecord::Migration[5.2]
   def change
     create_table :cars_parts do |t|
-      t.reference :car
-      t.reference :part
+      t.references :car
+      t.references :part
 
       t.timestamps
     end
